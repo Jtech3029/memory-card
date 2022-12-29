5a3ba0dcadcfca190e762869e03e9fe7132a8b16
CrypticAgro <jtech3029@gmail.com>
2022-12-29 16:53:58 -0700

    completed project; styling is non existent

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Card from './components/Card';
+import { imageList } from "./imgs/imageList";
 
 function App() {
+  const [hasLost, setHasLost] = useState(false);
+  const [hasWon, setHasWon] = useState(false);
+  const [pokemonArray, setPokemonArray] = useState(randomizeArray(imageList));
+  const [counter, setCounter] = useState(0);
+
+  /**
+   * on updates, checks if the win condition is satisfied and if true it sets to win
+   */
+  useEffect(() => {
+    if(hasLost === false){
+      for(let i = 0; i < pokemonArray.length; i++){
+        if(pokemonArray[i][2] === false) {
+          return;
+        }
+      }
+      setHasWon(true);
+    }
+  })
+
+  function resetGame() {
+    if(hasLost === true){
+      setHasLost(false);
+    }
+    else{
+      setHasWon(false);
+    }
+    setCounter(0);
+    setPokemonArray(randomizeArray(imageList));
+  }
+  function randomizeArray(array) {
+    let newArray = array.slice(0);
+  
+    for (var i = newArray.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = newArray[i];
+      newArray[i] = newArray[j];
+      newArray[j] = temp;
+    }
+  return newArray;
+  
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>
+      {hasLost === false && hasWon === false &&
+      <div>
+        {counter}
+      </div>
+      }
+      {hasLost === false && hasWon === false &&
+      pokemonArray.map((object, i) => <Card 
+      image={pokemonArray[i][0]} 
+      pokemonName={pokemonArray[i][1]} 
+      key={i} 
+      index={i} 
+      update={setPokemonArray} 
+      pokemonArray={pokemonArray}
+      randomize={randomizeArray}
+      counter={counter}
+      setCounter={setCounter}
+      endGame={setHasLost}/>)}
+      {hasLost === true &&
+      <div>
+      u suck  
+      <button onClick={resetGame}>
+      play again?
+      </button>  
+      </div>}
+      {hasWon === true &&
+      <div>
+      u dont suck
+      <button onClick={resetGame}>
+      play again?
+      </button>  
+      </div>
+      }
     </div>
   );
+  
 }
+
+
+
 
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,52 @@
+function Card(props) {
+    /**
+     * when the component has been clicked, it updates the component and checks if the component has been pressed twice
+     * @param {object} e 
+     */
+    function hasClicked(e) {
+        props.update(checkIfTrue(props.pokemonArray));
+        props.setCounter(props.counter + 1);
+    }
+    /**
+     * 
+     * @param {array} array 
+     * @returns a randomized array that also updates whether the card has been pressed or not
+     */
+    function checkIfTrue(array) {
+        if(array[props.index][2] === false){
+            let newArray = array.splice(0);
+            newArray[props.index][2] = true;
+            return props.randomize(newArray);
+        }
+        props.endGame(true);
+    }
+    return (
+        <div id={props.pokemonName} onClick={hasClicked}>
+            <img src={props.image} alt="A pokemon"/>
+            <div>{props.pokemonName}</div>
+        </div>
+    )
+}
+
+export default Card;
+
+/*
+function Card(props) {
+    useEffect(() => {
+       document.addEventListener("click", hasClicked)
+       return () => {document.removeEventListener(("click"), hasClicked);
+    }
+    })
+
+    function hasClicked(e) {
+        e.stopPropagation();
+        console.log(props.index);
+    }
+    return (
+        <div id={props.pokemonName}>
+            <img src={props.image} alt="A pokemon"/>
+            <div>{props.pokemonName}</div>
+        </div>
+    )
+}
+*/
